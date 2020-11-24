da4da4a1f8ac03621cff3133d991d30078499019
dolineanuvlad <vlad.dolineanu@rodeapps.com>
2020-11-24 10:43:24 +0200

    Funnel navigation

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -6,6 +6,7 @@ import cn from "classnames";
 import Logo from "components/common/Logo/Logo";
 import Breadcrumbs from "components/common/Breadcrumbs/Breadcrumbs";
 import { BREADCRUMBS_STEPS } from "constants/general";
+import { PERSONAL_INFORMATION_URL } from "constants/urls";
 
 type Props = RouteComponentProps;
 
@@ -30,16 +31,41 @@ export class Cart extends React.Component<Props, State> {
     this.context.updateCart({ id: id });
   };
 
-  render() {
+  renderDebug() {
     const cartData = this.context.cart;
+    return (
+      <React.Fragment>
+        [CONTEXT DEBUG AREA]
+        <br />
+        [CART]
+        <br />
+        Context cart data:
+        <br />
+        <pre>{JSON.stringify(cartData)}</pre>
+        <button onClick={this.updateCart} style={{ marginBottom: 20 }}>
+          Randomize cart id
+        </button>
+      </React.Fragment>
+    );
+  }
+
+  render() {
     return (
       <div className={cn("funnel-page", styles.Cart)}>
         <Logo className={styles.logo} />
         <Breadcrumbs steps={BREADCRUMBS_STEPS} className={styles.breadcrumbs} />
-        Context cart data
-        <br />
-        {JSON.stringify(cartData)}
-        <button onClick={this.updateCart}>Randomize cart id</button>
+        {this.renderDebug()}
+        <div className={styles.navigationContainer}>
+          <span />
+          <button
+            className="button large"
+            onClick={() => {
+              this.props.history.push(PERSONAL_INFORMATION_URL);
+            }}
+          >
+            Continue to Information
+          </button>
+        </div>
       </div>
     );
   }
--- src/components/PersonalInformation/PersonalInformation.tsx
+++ src/components/PersonalInformation/PersonalInformation.tsx
@@ -2,9 +2,10 @@ import Breadcrumbs from "components/common/Breadcrumbs/Breadcrumbs";
 import Logo from "components/common/Logo/Logo";
 import { BREADCRUMBS_STEPS } from "constants/general";
 import * as React from "react";
-import { RouteComponentProps, withRouter } from "react-router-dom";
+import { Link, RouteComponentProps, withRouter } from "react-router-dom";
 import styles from "./PersonalInformation.module.scss";
 import cn from "classnames";
+import { CART_URL, PAYMENT_URL } from "constants/urls";
 
 type Props = RouteComponentProps;
 
@@ -23,6 +24,20 @@ export class PersonalInformation extends React.Component<Props, State> {
         <Logo className={styles.logo} />
         <Breadcrumbs steps={BREADCRUMBS_STEPS} className={styles.breadcrumbs} />
         Personal Information
+        <div className={styles.navigationContainer}>
+          <Link to={CART_URL} className="link-button">
+            <i className="far fa-long-arrow-left" />
+            Return to cart
+          </Link>
+          <button
+            className="button large"
+            onClick={() => {
+              this.props.history.push(PAYMENT_URL);
+            }}
+          >
+            Continue to Payment
+          </button>
+        </div>
       </div>
     );
   }
