60d487d40770b00c747883b89c2755713ca94069
dolineanuvlad <vlad.dolineanu@rodeapps.com>
2020-12-04 12:09:25 +0200

    Finished Order Summary

--- src/components/OrderSummary/CartItem/CartItem.tsx
+++ src/components/OrderSummary/CartItem/CartItem.tsx
@@ -3,13 +3,12 @@ import * as React from "react";
 import styles from "./CartItem.module.scss";
 
 type Props = {
-  key?: string;
   cartItem: CartItemT;
 };
 
 export default function CartItem(props: Props) {
   return (
-    <div className={styles.CartItem} key={props.key}>
+    <div className={styles.CartItem}>
       <div className={styles.itemInfo}>
         <img
           className={styles.image}
