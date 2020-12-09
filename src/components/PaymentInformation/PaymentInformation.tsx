888246f191e02e4a483b671bf2c712e9f4c5ee54
Radu Durican <radu.durican@rodeapps.com>
2020-12-09 15:11:28 +0200

    MAT-21 UI for collapsed payment methods

--- src/components/PaymentInformation/PaymentInformation.tsx
+++ src/components/PaymentInformation/PaymentInformation.tsx
@@ -7,7 +7,9 @@ import styles from "./PaymentInformation.module.scss";
 import cn from "classnames";
 import { PERSONAL_INFORMATION_URL } from "constants/urls";
 import EncryptionNotice from "./EncryptionNotice/EncryptionNotice";
-import Input from "../common/Input/Input";
+import RadioButton from "../common/RadioButton/RadioButton";
+import applePayLogo from "../../assets/images/apple_pay_logo_black.svg";
+import payPalLogo from "../../assets/images/paypal_logo.svg";
 
 type Props = RouteComponentProps;
 
@@ -17,26 +19,82 @@ type State = {
 
 export class PaymentInformation extends React.Component<Props, State> {
   state = {
-    paymentInfo: {},
+    paymentInfo: "",
   };
 
   renderContactInfoSection = () => {
     return (
-      <div className={styles.infoSection}>
+      <div className={`${styles.infoSection} ${styles.paddingContainer}`}>
         <h3 className={styles.title}>Contact</h3>
         <div className={styles.changeButton}>Change</div>
-        <div className={styles.value}>johndoe@gmail.com</div>
+        <div className={cn("big-text", styles.value)}>johndoe@gmail.com</div>
       </div>
     );
   };
 
   renderShipToInfoSection = () => {
     return (
-      <div className={styles.infoSection}>
+      <div className={`${styles.infoSection} ${styles.paddingContainer}`}>
         <h3 className={styles.title}>Ship To</h3>
         <div className={styles.changeButton}>Change</div>
-        <div className={styles.value}>
+        <div className={cn("big-text", styles.value)}>
           236 West 30th Street 11th Floor, New York, NY 10001
+        </div>
+      </div>
+    );
+  };
+
+  renderPaymentInfoSection = () => {
+    return (
+      <div>
+        <h3 className="margin-top">Payment</h3>
+        <div className={styles.paddingContainer}>
+          <div className="row center-vertically">
+            <RadioButton
+              className={styles.radioButton}
+              value={this.state.paymentInfo}
+              option="credit-card"
+              onChange={(val: string) => {
+                this.setState({ paymentInfo: val });
+              }}
+            />
+            <div className="big-text">Credit Card</div>
+          </div>
+        </div>
+
+        <div className={styles.paddingContainer}>
+          <div className="row center-vertically">
+            <RadioButton
+              className={styles.radioButton}
+              value={this.state.paymentInfo}
+              option="pay-pal"
+              onChange={(val: string) => {
+                this.setState({ paymentInfo: val });
+              }}
+            />
+            <img
+              src={payPalLogo}
+              alt="Pay Pal"
+              className={styles.paymentLogoIcon}
+            />
+          </div>
+        </div>
+        <div className={styles.paddingContainer}>
+          <div className="row center-vertically">
+            <RadioButton
+              className={styles.radioButton}
+              value={this.state.paymentInfo}
+              option="apple-pay"
+              onChange={(val: string) => {
+                this.setState({ paymentInfo: val });
+              }}
+            />
+            <img
+              src={applePayLogo}
+              alt="Apple Pay"
+              className={styles.paymentLogoIcon}
+            />
+          </div>
         </div>
       </div>
     );
@@ -51,6 +109,7 @@ export class PaymentInformation extends React.Component<Props, State> {
 
         {this.renderContactInfoSection()}
         {this.renderShipToInfoSection()}
+        {this.renderPaymentInfoSection()}
 
         <div className={styles.navigationContainer}>
           <Link to={PERSONAL_INFORMATION_URL} className="link-button">
