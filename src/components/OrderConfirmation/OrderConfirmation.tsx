b96ca65b3a01abe1513ee51695d30be464f7b78c
marius-vlas <marius.vlas@rodeapps.com>
2020-12-09 12:12:42 +0200

    Done Order confirmation texts. Improved Recommendations responsiveness

--- src/components/OrderConfirmation/OrderConfirmation.tsx
+++ src/components/OrderConfirmation/OrderConfirmation.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import { Recommendations } from "../common/Recommendations/Recommendations";
 import Logo from "../common/Logo/Logo";
 import cn from "classnames";
-import styles from "../PersonalInformation/PersonalInformation.module.scss";
+import styles from "../OrderConfirmation/OrderConfirmation.module.scss";
 
 export class OrderConfirmation extends React.Component {
+  user = {
+    firstName: "John",
+  };
+  order = {
+    number: 2625117283,
+  };
   recommendations = [
     {
       id: 1,
@@ -44,11 +50,31 @@ export class OrderConfirmation extends React.Component {
       type: 1,
     },
   ];
-  user = {
-    firstName: "John",
-  };
-  order = {
-    number: 2625117283,
+
+  customerInfo = {
+    customerEmail: "johndoe@gmail.com",
+    paymentInfo: {
+      cardIssuer: "VISA",
+      cardNumber: "1234",
+      amount: 178.72,
+      currency: "$",
+    },
+    shippingAddress: {
+      name: "John Doe",
+      companyName: "Vaudeville Ventures",
+      address: "236 W 30th Street 11th Floor",
+      city: "New York",
+      state: "NY",
+      postalCode: "10001",
+    },
+    billingAddress: {
+      name: "John Doe",
+      companyName: "Vaudeville Ventures",
+      address: "236 W 30th Street 11th Floor",
+      city: "New York",
+      state: "NY",
+      postalCode: "10001",
+    },
   };
 
   constructor(props: any) {
@@ -64,19 +90,61 @@ export class OrderConfirmation extends React.Component {
       <div className={cn("funnel-page")}>
         <Logo />
         <div className={styles["confirmation-text"]}>
-          <div className={styles["order-number"]}>
-            Order {this.order.number}
+          <div
+            className={cn(
+              styles["order-number"],
+              styles["confirmation-text-row"],
+              "text-color-light font-size-md"
+            )}
+          >
+            Order #{this.order.number}
           </div>
-          <div className={styles["thank-you"]}>
-            Thank you {this.user.firstName}
+          <h2
+            className={cn(
+              styles["thank-you"],
+              styles["confirmation-text-row"],
+              "font-weight-medium"
+            )}
+          >
+            Thank you, {this.user.firstName}!
+          </h2>
+          <div
+            className={cn(
+              styles["confirmation-details"],
+              styles["confirmation-text-row"]
+            )}
+          >
+            Your order is confirmed. You’ll recieve an email when your order has
+            shipped.
+          </div>
+          <div
+            className={cn(
+              styles["pill"],
+              styles["confirmation-text-row"],
+              "font-size-md"
+            )}
+          >
+            <i className="far fa-shipping-fast font-size-xxl" />
+            <span>
+              All Design Shop orders are shipped
+              <span className="font-weight-bold">&nbsp;FREE&nbsp;</span>via
+              FedEx Priority Overnight!
+            </span>
           </div>
         </div>
 
+        <h2 className={styles["section-title"]}>
+          Based on your order, we think you'll love these:
+        </h2>
         <Recommendations
-          title="Based on your order, we think you'll love these:"
+          className={styles["Recommendations"]}
           recommendations={this.recommendations}
           recommendationClick={this.recommendationClick}
         />
+        <h2 className={styles["section-title"]}>Customer Information</h2>
+        <div className={styles["customer-information"]}>
+          <div className={styles["customer-information"]}></div>
+        </div>
       </div>
     );
   }
--- src/components/common/Recommendations/Recommendations.tsx
+++ src/components/common/Recommendations/Recommendations.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import styles from "./Recommendations.module.scss";
+import cn from "classnames";
 import { RecommendationCard } from "../RecommendationCard/RecommendationCard";
 
 type Props = {
-  title?: string;
   recommendations: { id: number; [key: string]: any }[];
   recommendationClick: (productId: number) => any;
+  className: string;
 };
 
 export function Recommendations(props: Props) {
   if (props.recommendations?.length) {
     return (
-      <div className={styles.Recommendations}>
-        <div className={styles.title}>{props.title || "Recommendations:"}</div>
+      <div className={cn(styles.Recommendations, props.className)}>
         <div className={styles["recommendations-grid"]}>
           {props.recommendations.map((r) => (
             <RecommendationCard
