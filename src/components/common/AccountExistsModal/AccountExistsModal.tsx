3ee1429b76fb1baf736b699291e31c8bef6e9dde
Radu Durican <radu.durican@rodeapps.com>
2021-02-14 12:56:05 +0200

    MAT-104 Finish mock UI for Account Already exists modal

--- src/components/common/AccountExistsModal/AccountExistsModal.tsx
+++ src/components/common/AccountExistsModal/AccountExistsModal.tsx
@@ -4,11 +4,23 @@ import { RouteComponentProps } from "react-router-dom";
 import { disableBodyScroll, enableBodyScroll } from "body-scroll-lock";
 import cn from "classnames";
 import styles from "./AccountExistsModal.module.scss";
+import Input from "components/common/Input/Input";
 
 type Props = RouteComponentProps;
 const ACCOUNT_EXISTS_CONTENT_ID = "accountExistsContentId";
 
-export class AccountExistsModal extends React.Component<any, any> {
+type State = {
+  login: {
+    password: string;
+  };
+  loginRequestError: string;
+  loginErrors: {
+    password: string | null;
+  };
+  isLoading: boolean;
+};
+
+export class AccountExistsModal extends React.Component<any, State> {
   static contextType = AppContext;
   context!: AppContextState;
 
@@ -16,6 +28,16 @@ export class AccountExistsModal extends React.Component<any, any> {
 
   constructor(props: Props) {
     super(props);
+    this.state = {
+      login: {
+        password: "",
+      },
+      loginErrors: {
+        password: null,
+      },
+      loginRequestError: "",
+      isLoading: false,
+    };
   }
 
   onBackgroundClicked = () => {
@@ -59,9 +81,48 @@ export class AccountExistsModal extends React.Component<any, any> {
           <div className={styles.closeButton} onClick={this.closeModal} />
           <div className={styles.modalContent}>
             <div className={styles.title}>Account Exists</div>
+            <div className={styles.subtitle}>
+              Looks like you already have an account. Sign in instead.
+            </div>
+
+            <div className={styles.userAvatar} />
+            <div className={styles.userName}>John Smith</div>
+            <div className={styles.userEmail}>john.smith@gmail.com</div>
+
+            <Input
+              className={styles.inputField}
+              placeholder="Password"
+              value={this.state.login.password}
+              type="password"
+              onChange={(val: string) => {
+                this.updatePassword(val);
+              }}
+              error={this.state.loginErrors.password}
+            />
+
+            <button className={styles.signInButton} onClick={() => {}}>
+              Sign In
+            </button>
+
+            <button className={styles.forgotPassword}>Forgot Password?</button>
+            <div className="horizontal-divider margin-top-big" />
+            <button className={styles.forgotPassword}>
+              Sign in with a different account
+            </button>
           </div>
         </div>
       </div>
     );
   }
+
+  updatePassword = (value: string) => {
+    this.setState({
+      login: {
+        password: value,
+      },
+      loginErrors: {
+        password: null,
+      },
+    });
+  };
 }
