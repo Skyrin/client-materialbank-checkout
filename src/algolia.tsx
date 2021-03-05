2818322e6f99a3e3e1b5f71455c567ab864c7863
dolineanuvlad <vlad.dolineanu@rodeapps.com>
2021-03-05 14:15:54 +0200

    MAT-163: Remove test line

--- src/algolia.tsx
+++ src/algolia.tsx
@@ -5,8 +5,6 @@ export const algoliaClient = algoliasearch(
   process.env.ALGOLIA_KEY || "2c2f5727fdeb1a428249dcb02dff6572"
 );
 
-export const algoliaClient2 = algoliasearch();
-
 export const algoliaProducts = algoliaClient.initIndex(
   "magento2_default_products"
 );
