9b8fb3287117ed712634ac4e00984d9ff093be64
Ioana Constantinescu <ioanaconstantinescu@ioana.local>
2021-03-10 13:11:45 +0200

    removed comments

--- src/components/CollectionsAndPalettes/CollectionsAndPalettes.tsx
+++ src/components/CollectionsAndPalettes/CollectionsAndPalettes.tsx
@@ -115,6 +115,7 @@ export default class CollectionsAndPalettes extends React.Component<
                   }}
                 />
               </Switch>
+              {/*The commonArea element is added here in order to keep the AddToCart Button inside the Collection Cards container, also decide its position*/}
               <div className={"commonArea"}>
                 <MoreIdeas />
                 <ExploreTags
