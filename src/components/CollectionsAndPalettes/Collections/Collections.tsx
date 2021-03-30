0f4b378e215786c154ab56b3d326725c44b2d9d2
Ioana Constantinescu <ioanaconstantinescu@192-168-0-104.rdsnet.ro>
2021-03-30 16:34:26 +0300

    collection grid cover

--- src/components/CollectionsAndPalettes/Collections/Collections.tsx
+++ src/components/CollectionsAndPalettes/Collections/Collections.tsx
@@ -8,18 +8,7 @@ import UploadCard from "../common/UploadCard/UploadCard";
 import Loader from "components/common/Loader/Loader";
 import MoreIdeas from "../common/MoreIdeas/MoreIdeas";
 
-interface State {
-  card: {
-    id: number;
-    type: string;
-    title1: string;
-    title2: string;
-    collaborators: any;
-    imagePath: string;
-  }[];
-}
-
-export default class Collections extends React.Component<any, State> {
+export default class Collections extends React.Component<any, any> {
   static contextType = AppContext;
   context!: AppContextState;
   modalTarget = null;
@@ -29,24 +18,6 @@ export default class Collections extends React.Component<any, State> {
 
   constructor(props) {
     super(props);
-    this.state = {
-      card: [
-        {
-          id: 1,
-          type: "collection",
-          title1: "collection",
-          title2: "Rhonda Roomdesigner",
-          collaborators: [
-            "https://vanishingportrait.com/wp-content/uploads/2019/05/tiffanytrenda-vanishingportrait-identity.jpg",
-            "https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg?size=626&ext=jpg",
-            "https://i.pinimg.com/originals/9c/a9/b2/9ca9b293ed52b3a124b802449eb653d0.jpg",
-            "https://www.adobe.com/content/dam/cc/us/en/creativecloud/photography/discover/portrait-photography/CODERED_B1_portrait_photography-P4a_438x447.jpg.img.jpg",
-          ],
-          imagePath:
-            "https://upload.wikimedia.org/wikipedia/commons/7/76/Color_icon_violet_v2.svg",
-        },
-      ],
-    };
   }
 
   render() {
--- src/components/CollectionsAndPalettes/common/CollectionCard/CollectionCard.tsx
+++ src/components/CollectionsAndPalettes/common/CollectionCard/CollectionCard.tsx
@@ -3,6 +3,7 @@ import * as React from "react";
 import styles from "../../common/ItemCard/ItemCard.module.scss";
 import Collaborators from "../Collaborators/Collaborators";
 import cn from "classnames";
+import { forEach, get } from "lodash-es";
 
 interface Props {
   collection: CollectionT;
@@ -17,16 +18,45 @@ export default class ItemCard extends React.Component<Props, any> {
     return `Private`;
   };
 
+  getImagePath = (item) => {
+    const { collection } = this.props;
+    if (item.upload) {
+      return item.upload.url;
+    } else if (item.material) {
+      const algoliaProduct = this.context.productsCache.getProduct(
+        item.material.sku
+      );
+      return get(
+        algoliaProduct,
+        "data.thumbnail_url",
+        "https://dev.design.shop/static/version1613493863/frontend/Magento/luma/en_US/Magento_Catalog/images/product/placeholder/image.jpg"
+      );
+    }
+  };
+
   render() {
     const { collection } = this.props;
-
-    const imagePath =
+    const imagePathEmpty =
       "https://upload.wikimedia.org/wikipedia/commons/7/76/Color_icon_violet_v2.svg";
-
     return (
-      <div className={styles.itemCard}>
-        <div className={styles.imageContainer}>
-          <img src={imagePath} alt="" />
+      <div className={cn(styles.itemCard, styles.collectionCard)}>
+        <div
+          className={cn(styles.imageContainer, {
+            singleImg: collection.items.length === 1,
+            twoImg: collection.items.length === 2,
+            multipleImg: collection.items.length > 2,
+          })}
+        >
+          {collection.items.slice(0, 4).map((collectionItem) => {
+            return (
+              <img
+                key={collectionItem.id}
+                src={this.getImagePath(collectionItem)}
+                className={styles.image}
+              />
+            );
+          })}
+          {collection.items.length === 0 && <img src={imagePathEmpty} alt="" />}
         </div>
         <div
           className={cn(
